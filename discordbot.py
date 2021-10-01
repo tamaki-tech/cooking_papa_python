ff2a9c34dadc34e5959cbcf65234f7b55ab60434
tamaki_tech <oshaburimegane@gmail.com>
2021-10-02 08:43:40 +0900

    クッキングパパ更新日の通知機能を実装

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,68 @@
-from discord.ext import commands
+from discord.ext import tasks
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from os import getenv
-import traceback
+import discord
+import time
+import re
 
-bot = commands.Bot(command_prefix='/')
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+client = discord.Client()
+channel_sent = None
+last_update = None
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+def scrape_publish_date():
+    driver_path = "./chromedriver"
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument('--proxy-server="direct://"')
+    options.add_argument("--proxy-bypass-list=*")
+    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(executable_path=driver_path, options=options)
+
+    driver.get("https://comic-days.com/episode/13932016480031248230")
+
+    title = driver.find_element_by_class_name("episode-header-title")
+    driver.execute_script("arguments[0].scrollIntoView();", title)
+
+    time.sleep(5)
+
+    next_update_date = driver.find_element_by_xpath("//p[@class='episode-read-date']")
+    return next_update_date.text
+
+
+@tasks.loop(minutes=10)
+async def loop():
+    if channel_sent is not None:
+        global last_update
+        publish_date = re.sub(r"\D", "", scrape_publish_date())
+        if last_update != publish_date:
+            await channel_sent.send(
+                "@everyone 今日はクッキングパパの更新日だぞ！\nhttps://comic-days.com/episode/13932016480031248230"
+            )
+            last_update = publish_date
+        else:
+            await channel_sent.send("今日は違います")
+
+
+
+@client.event
+async def on_ready():
+    global channel_sent
+    channel_id = getenv('CHANNEL_ID')
+    channel_sent = client.get_channel(channel_id)
+
+    global last_update
+    last_update = re.sub(r"\D", "", scrape_publish_date())
+
+    await channel_sent.send(
+        "クッキングパパの無料公開日をお知らせするぞ！\n次回更は" + scrape_publish_date() + "だ！"
+    )
+    loop.start()
 
 
 token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+client.run(token)
